dc0614784afbe814c89f068247e37dca24328862
Cristiano Coelho <cristianocca@hotmail.com>
2020-04-10 15:23:05 -0300

    fix imports, remove ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export * from "./PanoramaView";
+import * as React from "react";
+import { requireNativeComponent, Platform } from "react-native";
+
+interface Props {
+  imageUrl: string;
+  dimensions?: { width: number; height: number }; // Android-only
+  inputType?: "mono" | "stereo"; // Android-only
+  enableTouchTracking?: boolean;
+  onImageLoadingFailed?: () => void;
+  onImageLoaded?: () => void;
+  style: ViewStyle;
+}
